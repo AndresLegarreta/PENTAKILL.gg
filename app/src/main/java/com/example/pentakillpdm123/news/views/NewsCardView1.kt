1411de6136c7d3aecf0bb542cb90935c80b92449
ULSA <ulsa@iMac-de-ULSA.local>
2023-10-19 17:59:29 -0600

    Se agregan titulos a las vistas de las news

--- app/src/main/java/com/example/pentakillpdm123/news/views/NewsCardView1.kt
+++ app/src/main/java/com/example/pentakillpdm123/news/views/NewsCardView1.kt
@@ -3,10 +3,14 @@ package com.example.pentakillpdm123.news.views
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
+
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -15,19 +19,30 @@ import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.pentakillpdm123.R
 
 @Composable
 fun NewsCardView1(){
-    Box(modifier = Modifier
-        .height(150.dp)
-        .width(350.dp)
-        .clip(RoundedCornerShape(20.dp))
-        .border(width = 2.dp, color = Color.Black, shape = RoundedCornerShape(20.dp))) {
+    Column {
+        Text(
+            text = "New Skinline - La Ilusion",
+            color = Color.Black, // Color del texto
+            fontSize = 24.sp // Tamaño del texto en sp
+
+        )
+        Spacer(modifier = Modifier.height(5.dp))
+        Box(modifier = Modifier
+            .height(150.dp)
+            .width(350.dp)
+            .clip(RoundedCornerShape(20.dp))
+            .border(width = 2.dp, color = Color.Black, shape = RoundedCornerShape(20.dp))) {
 
 
-        Image(painter = painterResource(id = R.drawable.rennata), contentDescription = "imagen", contentScale = ContentScale.FillWidth, modifier = Modifier.fillMaxSize())
+            Image(painter = painterResource(id = R.drawable.rennata), contentDescription = "imagen", contentScale = ContentScale.FillWidth, modifier = Modifier.fillMaxSize())
+        }
     }
+
 
 }
 
